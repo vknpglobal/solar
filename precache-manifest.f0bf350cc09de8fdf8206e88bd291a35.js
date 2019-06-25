fdd86c2862d4b8f0345b5c85566f12d7b7c0915e
Vikash Chauhan <Vikash.Chauhan@SERVERNP.COM>
2019-06-25 15:02:28 +0530

    solar website first commit

---
+++ precache-manifest.f0bf350cc09de8fdf8206e88bd291a35.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = [
+  {
+    "revision": "79231c87f57a1c039accf69ee4988fc4",
+    "url": "/static/media/pattern.79231c87.png"
+  },
+  {
+    "revision": "1475d9c04456343a89ac",
+    "url": "/static/css/main.2e9fcee6.chunk.css"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "dcc8149dfaa9d426c0d9",
+    "url": "/static/js/1.dcc8149d.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "09e7f000129dfa7ce17ecce3ee8022ce",
+    "url": "/static/media/smile.09e7f000.png"
+  },
+  {
+    "revision": "3898f76f342a61a0ade2e93c5b6ea90c",
+    "url": "/static/media/solar-logo.3898f76f.svg"
+  },
+  {
+    "revision": "99195a1c393473688c95cac54c277e7c",
+    "url": "/static/media/bg-1.99195a1c.jpg"
+  },
+  {
+    "revision": "e06178c3f7ac0662aa2375f40699f893",
+    "url": "/static/media/bg-2.e06178c3.jpg"
+  },
+  {
+    "revision": "d7055a9ddb7e3ffef267303477fd1d68",
+    "url": "/static/media/bg-3.d7055a9d.jpg"
+  },
+  {
+    "revision": "0c99eba912fd5950a10e4e13035e1664",
+    "url": "/static/media/services.0c99eba9.jpg"
+  },
+  {
+    "revision": "eeccdc6b28cf70cce1a8409fed82b77d",
+    "url": "/static/media/testimonials.eeccdc6b.jpg"
+  },
+  {
+    "revision": "8114c320c2b80c087967d6d7de02b280",
+    "url": "/static/media/who-we-are.8114c320.jpg"
+  },
+  {
+    "revision": "1475d9c04456343a89ac",
+    "url": "/static/js/main.1475d9c0.chunk.js"
+  },
+  {
+    "revision": "68d620ca38d5ce507f9f6704dd35b98f",
+    "url": "/static/media/breadcrumbs.68d620ca.jpg"
+  },
+  {
+    "revision": "f357237d4fc30f9dfcd82679ab8c2b05",
+    "url": "/static/media/banner1.f357237d.jpg"
+  },
+  {
+    "revision": "7a626d9ca5df48a5f3f532f23d816ca2",
+    "url": "/static/media/1.7a626d9c.png"
+  },
+  {
+    "revision": "1271aafe43620b4762edd94d8b80e1a9",
+    "url": "/static/media/2.1271aafe.png"
+  },
+  {
+    "revision": "66c1aa44fbfa0ec1f958337cf6824d2a",
+    "url": "/static/media/4.66c1aa44.png"
+  },
+  {
+    "revision": "b5580b1d01f00b5f346adef8f5c8d870",
+    "url": "/static/media/5.b5580b1d.png"
+  },
+  {
+    "revision": "ce53de5f0974dc49d674ac95b9d17ea4",
+    "url": "/static/media/6.ce53de5f.png"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "dcc8149dfaa9d426c0d9",
+    "url": "/static/css/1.45804495.chunk.css"
+  },
+  {
+    "revision": "a28871c487826dd1c6f54b8c0f9e1dbe",
+    "url": "/index.html"
+  }
+];
